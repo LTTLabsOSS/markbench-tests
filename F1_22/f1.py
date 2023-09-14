fb575e3d073db50dd288103474f5ea74453d01af
Derek Hirotsu <132305781+derek-hirotsu@users.noreply.github.com>
2023-09-14 16:31:08 -0700

    Audit F1 22 (#6)
    * remove unused imports
    
    * remove deprectated cv2 utils
    
    * Add readme to f1_22
    
    * remove graphics_preset from report json in f1_22
    
    * pep8 formatting
    
    * add start and end time recording to tests
    
    * implement resolution retrieval
    
    * pep8 lint

--- F1_22/f1.py
+++ F1_22/f1.py
@@ -1,104 +1,86 @@
-import json
+"""F1 22 Test script"""
 import logging
+import sys
 import os.path
 import time
-from f1_22_utils import get_args
-import pydirectinput as user
 from subprocess import Popen
-import mss
-import cv2
-import requests
-import numpy as np
-import sys
-
+import pydirectinput as user
+from f1_22_utils import get_args
 from f1_22_utils import get_resolution, remove_intro_videos
 
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+#pylint: disable=wrong-import-position
 
 from harness_utils.keras_service import KerasService
-from harness_utils.logging import format_resolution, seconds_to_milliseconds, setup_log_directory, write_report_json, DEFAULT_LOGGING_FORMAT, DEFAULT_DATE_FORMAT
+from harness_utils.logging import (
+    format_resolution,
+    seconds_to_milliseconds,
+    setup_log_directory,
+    write_report_json,
+    DEFAULT_LOGGING_FORMAT,
+    DEFAULT_DATE_FORMAT,
+)
 from harness_utils.process import terminate_processes
+
+#pylint: enable=wrong-import-position
 
 SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 LOG_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "run")
 STEAM_GAME_ID = 1692250
 STEAM_PATH = os.path.join(os.environ["ProgramFiles(x86)"], "steam")
 STEAM_EXECUTABLE = "steam.exe"
-
-video_path = r'C:\Program Files (x86)\Steam\steamapps\common\F1 22\videos'
+VIDEO_PATH = r"C:\Program Files (x86)\Steam\steamapps\common\F1 22\videos"
 
 skippable = [
-    os.path.join(video_path, "attract.bk2"),
-    os.path.join(video_path, "cm_f1_sting.bk2")
+    os.path.join(VIDEO_PATH, "attract.bk2"),
+    os.path.join(VIDEO_PATH, "cm_f1_sting.bk2"),
 ]
 
 
 def wait_for_word(word: str, attempts: int = 5, delay_seconds: int = 1) -> bool:
+    """Gets results of search for word in screenshot sent to Keras Service."""
     for _ in range(attempts):
         result = kerasService.capture_screenshot_find_word(word)
-        if result != None:
+        if result is not None:
             return True
         time.sleep(delay_seconds)
     return False
 
 
 def start_game():
+    """Starts the game via steam command."""
     steam_run_arg = "steam://rungameid/" + str(STEAM_GAME_ID)
     start_cmd = STEAM_PATH + "\\" + STEAM_EXECUTABLE
-    logging.info(start_cmd + " " + steam_run_arg)
+    logging.info("%s %s", start_cmd, steam_run_arg)
     return Popen([start_cmd, steam_run_arg])
 
 
-# ASSUMPTIONS
-# 1. We are doing 3 laps
-def run_benchmark():
-    t1 = time.time()
-    remove_intro_videos(skippable)
-    start_game()
-
-    ###
-    # GET THROUGH ALL THE STARTING BS
-    ###
-
-    # starting movie crap
-    time.sleep(20)
-
-    # press space through the warnings
-    iterations = 0
-    while iterations < 5:
-        user.press("space")
-        iterations += 1
-        time.sleep(1)
-
+def navigate_overlay():
+    """Simulate inputs to navigate ingame overlay."""
     # if steam ingame overlay is disabled it will be a an okay to press
     okay_present = wait_for_word(word="okay", attempts=5, delay_seconds=1)
     if okay_present:
         user.press("enter")
-            
+
     time.sleep(3)
 
     # if steam ingame overlay is enabled we have to press escape and enter
     if not okay_present:
         please_present = wait_for_word(word="please", attempts=5, delay_seconds=1)
-        if please_present != None:
-            user.press('esc')
+        if please_present is not None:
+            user.press("esc")
             time.sleep(0.5)
-            user.press('down')
+            user.press("down")
             time.sleep(0.5)
-            user.press('enter')
+            user.press("enter")
             time.sleep(0.5)
-            
+
     time.sleep(3)
 
-    options_present = wait_for_word("options", attempts=5, delay_seconds=1)
-    if not options_present:
-        print("Didn't land on the main menu!")
-        exit(1)
 
-    ###
-    # SHOULD BE ON MAIN MENU RIGHT NOW
-    ###
-
+def navigate_menu():
+    """Simulate inputs to navigate to benchmark option."""
     # Enter options
     user.press("down")
     time.sleep(0.5)
@@ -143,68 +125,103 @@ def run_benchmark():
     time.sleep(0.5)
     user.press("enter")
 
+
+def run_benchmark():
+    """Runs the actual benchmark."""
+    setup_start_time = time.time()
+    remove_intro_videos(skippable)
+    start_game()
+
+    time.sleep(20)
+
+    # press space through the warnings
+    for _ in range(5):
+        user.press("space")
+        time.sleep(1)
+
+    navigate_overlay()
+
+    options_present = wait_for_word("options", attempts=5, delay_seconds=1)
+    if not options_present:
+        print("Didn't land on the main menu!")
+        sys.exit(1)
+
+    navigate_menu()
+
+    elapsed_setup_time = round(time.time() - setup_start_time, 2)
+    logging.info("Setup took %f seconds", elapsed_setup_time)
+
+    test_start_time = time.time()
+
     # sleep 6.75 for 3 laps
     time.sleep(330)
 
-     # if steam ingame overlay is disabled it will be a an okay to press
+    # if steam ingame overlay is disabled it will be a an okay to press
     okay_present = wait_for_word(word="okay", attempts=5, delay_seconds=1)
     if okay_present:
         user.press("enter")
-            
+
     time.sleep(3)
 
     if not okay_present:
         # if steam ingame overlay is enabled we have to press escape and enter
         please_present = wait_for_word(word="please", attempts=5, delay_seconds=1)
-        if please_present != None:
-            user.press('esc')
+        if please_present is not None:
+            user.press("esc")
             time.sleep(0.5)
-            user.press('down')
+            user.press("down")
             time.sleep(0.5)
-            user.press('enter')
+            user.press("enter")
             time.sleep(0.5)
-            
+
     time.sleep(3)
 
     results_present = wait_for_word(word="results", attempts=5, delay_seconds=1)
     if not results_present:
-        print("Results screen was not found! Did harness not wait long enough? Or test was too long?")
-        exit(1)
+        print(
+            "Results screen was not found! Did harness not wait long enough? Or test was too long?"
+        )
+        sys.exit(1)
 
-    """
-    Exit
-    """
+    elapsed_test_time = round(time.time() - test_start_time, 2)
+
     terminate_processes("F1")
-    return 0, 0 # Start/End time stub
+    return test_start_time, elapsed_test_time
 
 
 setup_log_directory(LOG_DIRECTORY)
 
-logging.basicConfig(filename=f'{LOG_DIRECTORY}/harness.log',
-                    format=DEFAULT_LOGGING_FORMAT,
-                    datefmt=DEFAULT_DATE_FORMAT,
-                    level=logging.DEBUG)
+logging.basicConfig(
+    filename=f"{LOG_DIRECTORY}/harness.log",
+    format=DEFAULT_LOGGING_FORMAT,
+    datefmt=DEFAULT_DATE_FORMAT,
+    level=logging.DEBUG,
+)
 console = logging.StreamHandler()
 formatter = logging.Formatter(DEFAULT_LOGGING_FORMAT)
 console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+logging.getLogger("").addHandler(console)
 
 args = get_args()
-kerasService = KerasService(args.keras_host, args.keras_port, os.path.join(LOG_DIRECTORY, "screenshot.jpg"))
+kerasService = KerasService(
+    args.keras_host, args.keras_port, os.path.join(LOG_DIRECTORY, "screenshot.jpg")
+)
 
 try:
     start_time, end_time = run_benchmark()
     width, height = get_resolution()
-    result = {
+    report = {
         "resolution": format_resolution(width, height),
-        "graphics_preset": 'current',
         "start_time": seconds_to_milliseconds(start_time),
-        "end_time": seconds_to_milliseconds(end_time)
+        "end_time": seconds_to_milliseconds(end_time),
     }
 
-    write_report_json(LOG_DIRECTORY, "report.json", result)
+    write_report_json(LOG_DIRECTORY, "report.json", report)
+
+#pylint: disable=broad-exception-caught
+
 except Exception as e:
     logging.error("Something went wrong running the benchmark!")
     logging.exception(e)
     terminate_processes("F1")
-    exit(1)
+    sys.exit(1)
--- F1_22/f1_22_utils.py
+++ F1_22/f1_22_utils.py
@@ -1,23 +1,49 @@
+"""Utility functions supporting F1 22 test script."""
 from argparse import ArgumentParser
 import os
-
-# Stub
-
+import re
 
 def get_resolution() -> tuple[int]:
-    return 0, 0
+    """Gets resolution width and height from local xml file created by game."""
+    username = os.getlogin()
+    config_path = f"C:\\Users\\{username}\\Documents\\My Games\\F1 22\\hardwaresettings"
+    config_filename = "hardware_settings_config.xml"
+    resolution = re.compile(r"<resolution width=\"(\d+)\" height=\"(\d+)\"")
+    cfg = f"{config_path}\\{config_filename}"
+    height = 0
+    width = 0
+    with open(cfg, encoding="utf-8") as file:
+        lines = file.readlines()
+        for line in lines:
+            height_match = resolution.search(line)
+            width_match = resolution.search(line)
+            if height_match is not None:
+                height = height_match.group(2)
+            if width_match is not None:
+                width = width_match.group(1)
+    return (width, height)
 
 
 def get_args() -> any:
+    """Retrieve parsed command line arguments."""
     parser = ArgumentParser()
-    parser.add_argument("--kerasHost", dest="keras_host",
-                        help="Host for Keras OCR service", required=True)
-    parser.add_argument("--kerasPort", dest="keras_port",
-                        help="Port for Keras OCR service", required=True)
+    parser.add_argument(
+        "--kerasHost",
+        dest="keras_host",
+        help="Host for Keras OCR service",
+        required=True,
+    )
+    parser.add_argument(
+        "--kerasPort",
+        dest="keras_port",
+        help="Port for Keras OCR service",
+        required=True,
+    )
     return parser.parse_args()
 
 
 def remove_intro_videos(file_paths: list[str]) -> None:
+    """Remove video files from paths to speed up game startup."""
     for video in file_paths:
         try:
             os.remove(video)
