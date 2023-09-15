b1b105fd88c2cd92743edfcd67969cfa3ef0bad2
Derek Hirotsu <132305781+derek-hirotsu@users.noreply.github.com>
2023-09-15 14:29:30 -0700

    Remove ACV (#14)
    * remove pyproject.toml
    
    * remove presets
    
    * remove preset args
    
    * remove deprecated template tests
    
    * Remove deprecated features related to configuring settings
    
    * pep8 lint
    
    * remove ac valhalla

--- deprecated/acvalhalla/arg_utils.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-
-
-def read_args():
-    parser = ArgumentParser()
-    parser.add_argument("-p", "--preset", dest="preset",
-                        help="graphics preset", metavar="preset", required=False)
-    args = parser.parse_args()
-    return args
