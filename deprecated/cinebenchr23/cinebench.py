0fcf019f1207a8d2f91ad3a8c650371c0b66866f
nharris-lmg <105814489+nharris-lmg@users.noreply.github.com>
2023-09-18 09:39:25 -0700

    Update cinebenchr23 harness (#18)
    * Update harness
    
    * Update readme
    
    * audit blender benchmark
    
    * Update cinebenchr23, its deprecated

--- deprecated/cinebenchr23/cinebench.py
+++ deprecated/cinebenchr23/cinebench.py
@@ -1,40 +1,35 @@
-
-from multiprocessing.sharedctypes import Value
+'''CinebenchR23 test script'''
 import subprocess
 import os
 import logging
 import re
 import json
 from argparse import ArgumentParser
+import sys
 
-"""
-Parsin` arguments!
-"""
+
 parser = ArgumentParser()
-parser.add_argument("-p", "--test", dest="test", help="test", metavar="test", required=True)
-parser.add_argument("-r", "--minduration", dest="minduration", help="minduration", metavar="minduration", required=False)
+parser.add_argument("-p", "--test", dest="test",
+                    help="test", metavar="test", required=True)
+parser.add_argument("-r", "--minduration", dest="minduration",
+                    help="minduration", metavar="minduration", required=False)
 args = parser.parse_args()
 
-"""
-Setting up some directories and logging what-have-yous
-"""
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 log_dir = os.path.join(script_dir, "run")
 if not os.path.isdir(log_dir):
     os.mkdir(log_dir)
-logging_format = '%(asctime)s %(levelname)-s %(message)s'
+LOGGING_FORMAT = '%(asctime)s %(levelname)-s %(message)s'
 logging.basicConfig(filename=f'{log_dir}/harness.log',
-                    format=logging_format,
+                    format=LOGGING_FORMAT,
                     datefmt='%m-%d %H:%M',
                     level=logging.DEBUG)
 console = logging.StreamHandler()
-formatter = logging.Formatter(logging_format)
+formatter = logging.Formatter(LOGGING_FORMAT)
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
-"""
-Running cinebench
-"""
 # See https://www.maxon.net/en/cinebench-tech-info for CLI options
 # g_CinebenchCpu1Test=true – runs only the Single Core test procedure
 # g_CinebenchCpuXTest=true – runs only the Multi Core test procedure
@@ -48,31 +43,30 @@ test_options = {
 }
 
 if args.test not in test_options:
-    logging.error(f"Unrecognized test: {args.test}")
-    exit(1)
+    logging.error("Unrecognized test %s", args.test)
+    sys.exit(1)
 
 opts = [test_options[args.test]]
 
 # isdigit() ensures the value has no decimal as well
-if args.minduration != None and not args.minduration.isdigit():
-    logging.error(f"Minimum duration must be a whole number: {args.minduration}")
-    exit(1)
-elif args.minduration != None:
+if args.minduration is not None and not args.minduration.isdigit():
+    logging.error(
+        "Minimum duration must be a whole number %s", args.minduration)
+    sys.exit(1)
+elif args.minduration is not None:
     opts = [*opts, f"g_CinebenchMinimumTestDuration={args.minduration}"]
 
 logging.info(opts)
-result = subprocess.run(["C:\\CinebenchR23\\Cinebench.exe", *opts], capture_output=True)
+result = subprocess.run(
+    ["C:\\CinebenchR23\\Cinebench.exe", *opts], capture_output=True, check=False)
 if result.returncode > 0:
     logging.error("Cinebench failed to run!")
     exit(result.returncode)
 
-"""
-Parsing the output of the run
-"""
 
-f = open(os.path.join(log_dir, "cinebenchlog.txt"), "wb")
-f.write(result.stdout)
-f.close
+with open(os.path.join(log_dir, "cinebenchlog.txt"), "wb") as f:
+    f.write(result.stdout)
+
 
 scorepattern = re.compile(r"^CB (\d+\.\d+) \(.+\)$")
 testtitlepattern = re.compile(r"Running (\w+) CPU Render Test\.\.\.")
@@ -89,7 +83,7 @@ for line in reversed(output.splitlines()):
         continue
 
 if len(scorestack) % 2 > 0:
-    logging.warn("Could not extract scores! Too many results found")
+    logging.warning("Could not extract scores! Too many results found")
 
 scores = []
 i = 0
@@ -97,11 +91,10 @@ while i < len(scorestack) - 1:
     scores.append(f"{scorestack[i + 1]}:{scorestack[i]}")
     i += 2
 
-result = {
+report = {
     "test": args.test,
     "score": scores
 }
 
-f = open(os.path.join(log_dir, "report.json"), "w")
-f.write(json.dumps(result))
-f.close()
+with open(os.path.join(log_dir, "report.json"), "w", encoding="utf-8") as file:
+    f.write(json.dumps(report))
