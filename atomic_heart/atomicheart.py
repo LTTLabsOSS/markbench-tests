67a4e6fe2bed670de0e674ab5f42bd5c48784aee
Derek Hirotsu <132305781+derek-hirotsu@users.noreply.github.com>
2023-09-19 15:02:29 -0700

    Audit Atomic Heart (#34)
    * pep8 lint
    
    * add readme; needs description
    
    * Update readme
    
    * fix readme

--- atomic_heart/atomicheart.py
+++ atomic_heart/atomicheart.py
@@ -1,18 +1,27 @@
+"""Atomic Heart test script"""
 from argparse import ArgumentParser
 import logging
 import os
-import pydirectinput as user
 import time
 from subprocess import Popen
 import sys
+import pydirectinput as user
 from utils import read_resolution, remove_intros, skippable
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
+#pylint: disable=wrong-import-position
 from harness_utils.steam import get_run_game_id_command, DEFAULT_EXECUTABLE_PATH as STEAM_PATH
 from harness_utils.process import terminate_processes
-from harness_utils.logging import setup_log_directory, write_report_json, DEFAULT_LOGGING_FORMAT, DEFAULT_DATE_FORMAT
+from harness_utils.logging import (
+    format_resolution,
+    seconds_to_milliseconds,
+    setup_log_directory,
+    write_report_json,
+    DEFAULT_LOGGING_FORMAT,
+    DEFAULT_DATE_FORMAT,
+)
 from harness_utils.keras_service import KerasService
+#pylint: enable=wrong-import-position
 
 STEAM_GAME_ID = 668580
 SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
@@ -25,40 +34,52 @@ PROCESS_NAME = "AtomicHeart"
 user.FAILSAFE = False
 
 def start_game():
+    """Starts the game via steam command."""
     steam_run_arg = get_run_game_id_command(STEAM_GAME_ID)
-    logging.info(STEAM_PATH + " " + steam_run_arg)
+    logging.info("%s %s", STEAM_PATH, steam_run_arg)
     return Popen([STEAM_PATH, steam_run_arg])
 
 
 def is_word_on_screen(word: str, attempts: int = 5, delay_seconds: int = 1) -> bool:
+    """Looks for given word a specified number of times"""
     for _ in range(attempts):
         result = kerasService.capture_screenshot_find_word(word)
-        if result != None:
+        if result is not None:
             return True
         time.sleep(delay_seconds)
     return False
 
+
 def start_screen() -> any:
+    """Looks for word press"""
     return is_word_on_screen(word="press", attempts=20, delay_seconds=1)
 
+
 def new_game() -> any:
+    """Looks for word new"""
     return is_word_on_screen(word="new", attempts=20, delay_seconds=1)
 
+
 def check_menu_continue() -> any:
+    """Looks for word continue"""
     return is_word_on_screen(word="continue", attempts=20, delay_seconds=1)
 
+
 def await_space_to_start() -> any:
+    """Looks for word continue"""
     return is_word_on_screen(word="cont", attempts=20, delay_seconds=1)
 
+
 def await_wicked() -> any:
+    """Looks for word wicked"""
     return is_word_on_screen(word="wicked", attempts=250, delay_seconds=0.1)
 
 
-
 def run_benchmark():
+    """Starts the benchmark"""
     remove_intros(skippable)
     start_game()
-    t1 = time.time()
+    setup_start_time = time.time()
 
     time.sleep(10)
 
@@ -67,7 +88,7 @@ def run_benchmark():
 
     if not start_screen():
         logging.info("Did not see start screen")
-        exit(1)
+        sys.exit(1)
 
     if new_game():
         if check_menu_continue():
@@ -90,49 +111,48 @@ def run_benchmark():
 
     time.sleep(10)
 
-    loading_screen= time.time()
-    while (True):
+    loading_screen = time.time()
+    while True:
         await_space_to_start()
-        if await_space_to_start:
+        if await_space_to_start():
             logging.info("Continue found. Continuing Run.")
-            user.press("space")           
+            user.press("space")
             time.sleep(5)
             break
-        elif time.time()-loading_screen>60:
+        if time.time()-loading_screen>60:
             logging.info("Did not see the option to continue. Check settings and try again.")
-            exit(1)
+            sys.exit(1)
         logging.info("Game hasn't loaded. Trying again in 5 seconds")
         time.sleep(5)
 
-    
-
-    t2 = time.time()
-    logging.info(f"Setup took {round((t2 - t1), 2)} seconds")
-    start_time = time.time()
+    elapsed_setup_time = round(time.time() - setup_start_time, 2)
+    logging.info("Setup took %f seconds", elapsed_setup_time)
+    test_start_time = time.time()
 
     time.sleep(230)  # wait for No Rest For the Wicked Quest
 
     # This looks for wicked
     wicked_check= time.time()
-    while (True):
+    while True:
         await_wicked()
-        if await_wicked:
-            logging.info("Wicked found. Ending Benchmark.")           
+        if await_wicked():
+            logging.info("Wicked found. Ending Benchmark.")
             time.sleep(5)
             break
-        elif time.time()-wicked_check>250:
-            logging.info("Wicked was not found! Did harness not wait long enough? Or test was too long?")
-            exit(1)
+        if time.time()-wicked_check>250:
+            logging.info(
+                "Wicked was not found! Did harness not wait long enough? Or test was too long?")
+            sys.exit(1)
         logging.info("Game hasn't finished the opening sequence. Trying again in 5 seconds")
         time.sleep(5)
 
-# End the run
-    end_time = time.time()
-    logging.info(f"Benchmark took {round((end_time - start_time), 2)} seconds")
+    test_end_time = time.time()
+    elapsed_test_time = round(test_end_time - test_start_time, 2)
+    logging.info("Benchmark took %f seconds", elapsed_test_time)
 
     # Exit
     terminate_processes(PROCESS_NAME)
-    return start_time, end_time
+    return test_start_time, test_end_time
 
 
 setup_log_directory(LOG_DIRECTORY)
@@ -156,18 +176,18 @@ kerasService = KerasService(args.keras_host, args.keras_port, os.path.join(
     LOG_DIRECTORY, "screenshot.jpg"))
 
 try:
-    start_time, endtime = run_benchmark()
+    start_time, end_time = run_benchmark()
     height, width = read_resolution()
-    result = {
-        "resolution": f"{width}x{height}",
-        "graphics_preset": "current",
-        "start_time": round((start_time * 1000)),  # seconds * 1000 = millis
-        "end_time": round((endtime * 1000))
+    report = {
+        "resolution": format_resolution(width, height),
+        "start_time": seconds_to_milliseconds(start_time),
+        "end_time": seconds_to_milliseconds(end_time)
     }
 
-    write_report_json(LOG_DIRECTORY, "report.json", result)
+    write_report_json(LOG_DIRECTORY, "report.json", report)
+#pylint: disable=broad-exception-caught
 except Exception as e:
     logging.error("Something went wrong running the benchmark!")
     logging.exception(e)
     terminate_processes(PROCESS_NAME)
-    exit(1)
+    sys.exit(1)
--- atomic_heart/utils.py
+++ atomic_heart/utils.py
@@ -1,9 +1,10 @@
+"""Atomic Heart utility functions"""
 import os
-import pydirectinput as user
 import sys
 import re
 import winreg
 import logging
+import pydirectinput as user
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
@@ -12,7 +13,8 @@ CONFIG_LOCATION = f"{APPDATA}\\AtomicHeart\\Saved\\Config\\WindowsNoEditor"
 CONFIG_FILENAME = "GameUserSettings.ini"
 PROCESS_NAME = "AtomicHeart"
 
-def InstallLocation() -> any:
+def get_install_location() -> any:
+    """Gets install location based on registry"""
     reg_path = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 668580'
     try:
         registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path, 0,
@@ -23,7 +25,7 @@ def InstallLocation() -> any:
     except WindowsError:
         return None
 
-video_path = os.path.join(InstallLocation(), "AtomicHeart\\Content\\Movies")
+video_path = os.path.join(get_install_location(), "AtomicHeart\\Content\\Movies")
 
 skippable = [
     os.path.join(video_path, "Launch_4k_60FPS_PS5.mp4"),
@@ -36,24 +38,25 @@ skippable = [
 user.FAILSAFE = False
 
 def remove_intros(file_paths: list[str]) -> None:
+    """Removes files from given paths"""
     for video in file_paths:
         try:
             os.remove(video)
-            logging.info(f"Removing video {video}")
+            logging.info("Removing video %s", video)
         except FileNotFoundError:
-            logging.info(f"Video already removed {video}")
+            logging.info("Video already removed %s", video)
             # If file not found, it has likely already been deleted before.
-            pass
 
 
 def read_resolution():
+    """Gets resolution width and height values from local file"""
     height_pattern = re.compile(r"ResolutionSizeY=(\d+)")
     width_pattern = re.compile(r"ResolutionSizeX=(\d+)")
     cfg = f"{CONFIG_LOCATION}\\{CONFIG_FILENAME}"
     height = 0
     width = 0
-    with open(cfg) as f:
-        lines = f.readlines()
+    with open(cfg, encoding="utf-8") as file:
+        lines = file.readlines()
         for line in lines:
             height_match = height_pattern.search(line)
             width_match = width_pattern.search(line)
@@ -62,5 +65,3 @@ def read_resolution():
             if width_match is not None:
                 width = width_match.group(1)
     return (height, width)
-
-
