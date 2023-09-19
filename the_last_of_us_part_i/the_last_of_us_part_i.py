0dd09adee91217dfd9ba2230b68ea26d461ad054
Derek Hirotsu <132305781+derek-hirotsu@users.noreply.github.com>
2023-09-19 13:58:20 -0700

    Audit TLoU Part I (#32)
    * pep8 lint
    
    * Update readme
    
    * update readme
    
    * typo
    
    * remove file

--- the_last_of_us_part_i/the_last_of_us_part_i.py
+++ the_last_of_us_part_i/the_last_of_us_part_i.py
@@ -1,49 +1,61 @@
-from argparse import ArgumentParser
+"""The Last of Us Part I test script"""
 import logging
 import os
-import pydirectinput as user
 import time
 from subprocess import Popen
 import sys
+import pydirectinput as user
 import pyautogui as gui
 
 from the_last_of_us_part_i_utils import get_args, get_resolution
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
+#pylint: disable=wrong-import-position
 from harness_utils.keras_service import KerasService
-from harness_utils.logging import *
+from harness_utils.logging import (
+    format_resolution,
+    seconds_to_milliseconds,
+    setup_log_directory,
+    write_report_json,
+    DEFAULT_LOGGING_FORMAT,
+    DEFAULT_DATE_FORMAT,
+)
 from harness_utils.process import terminate_processes
-from harness_utils.steam import get_run_game_id_command, get_registry_active_user, DEFAULT_EXECUTABLE_PATH as STEAM_PATH
+from harness_utils.steam import (
+  get_run_game_id_command, 
+  get_registry_active_user, 
+  DEFAULT_EXECUTABLE_PATH as STEAM_PATH
+)
+#pylint: enable=wrong-import-position
 
-STEAM_GAME_ID = 1888930 
+STEAM_GAME_ID = 1888930
 SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 LOG_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "run")
 PROCESS_NAME = "tlou"
-
-id = get_registry_active_user()
-config_path = os.path.join(os.environ["HOMEPATH"], "Saved Games" ,"The Last of Us Part I", "users", str(id), "screeninfo.cfg")
 
 user.FAILSAFE = False
 
 
 def start_game():
+    """Start game process via Steam"""
     steam_run_arg = get_run_game_id_command(STEAM_GAME_ID)
-    logging.info(STEAM_PATH + " " + steam_run_arg)
+    logging.info("%s %s", STEAM_PATH, steam_run_arg)
     return Popen([STEAM_PATH, steam_run_arg])
 
 
 def await_start_screen(attempts: int) -> bool:
+    """Wait for the start menu"""
     for _ in range(attempts):
         result = kerasService.capture_screenshot_find_word("press")
-        if result != None:
+        if result is not None:
             return True
         time.sleep(5)
-    
+
     return False
 
 
 def navigate_main_menu() -> None:
+    """Input to navigate main menu"""
     logging.info("Navigating main menu")
 
     # Enter TLOU menu
@@ -70,19 +82,21 @@ def navigate_main_menu() -> None:
 
 
 def await_load_game_menu(attempts: int) -> any:
+    """Wait for load game menu"""
     for _ in range(attempts):
         result = kerasService.capture_screenshot_find_word("yes")
-        if result != None:
+        if result is not None:
             return (result["x"], result["y"])
         time.sleep(1)
-    
+
     return None
 
 
 def await_fromy(attempts: int) -> bool:
+    """Wait for "from? in dialogue subtitles"""
     for _ in range(attempts):
         result = kerasService.capture_screenshot_find_word("fromy")
-        if result != None:
+        if result is not None:
             logging.info("Fromy found!!!")
             return True
         time.sleep(0.1)
@@ -91,16 +105,17 @@ def await_fromy(attempts: int) -> bool:
 
 
 def run_benchmark():
+    """Starts the benchmark"""
     start_game()
-    t1 = time.time()
+    setup_start_time = time.time()
 
     time.sleep(10)
 
     start_screen_found = await_start_screen(10)
-    
+
     if not start_screen_found:
         logging.info("Did not see start screen")
-        exit(1)
+        sys.exit(1)
 
     navigate_main_menu()
 
@@ -109,17 +124,17 @@ def run_benchmark():
 
     if not coords:
         logging.info("Did not load the save")
-        exit(1)
+        sys.exit(1)
 
     user.click(coords[0], coords[1])
     time.sleep(0.2)
     gui.mouseDown()
     time.sleep(0.2)
-    gui.mouseUp() 
+    gui.mouseUp()
 
-    t2 = time.time()
-    logging.info(f"Setup took {round((t2 - t1), 2)} seconds")
-    start_time = time.time()
+    elapsed_setup_time = round(time.time() - setup_start_time, 2)
+    logging.info("Setup took %f seconds", elapsed_setup_time)
+    test_start_time = time.time()
 
     time.sleep(150) # wait for black screen
 
@@ -127,20 +142,20 @@ def run_benchmark():
     fromy_found = await_fromy(250)
 
     if not fromy_found:
-        logging.info("Results screen was not found! Did harness not wait long enough? Or test was too long?")
-        exit(1)
+        logging.info(
+            "Results screen was not found! Did harness not wait long enough? Or test was too long?")
+        sys.exit(1)
 
     # Wait for black screen
     time.sleep(24)
 
-    # End the run
-    end_time = time.time()
-    logging.info(f"Benchark took {round((end_time - start_time), 2)} seconds")
+    test_end_time = time.time()
+    elapsed_test_time = round(test_end_time - test_start_time, 2)
+    logging.info("Benchmark took %f seconds", elapsed_test_time)
 
-    # Exit
     terminate_processes(PROCESS_NAME)
-    return start_time, end_time
-    
+    return test_start_time, test_end_time
+
 
 setup_log_directory(LOG_DIRECTORY)
 
@@ -154,21 +169,27 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
 args = get_args()
-kerasService = KerasService(args.keras_host, args.keras_port, os.path.join(LOG_DIRECTORY, "screenshot.jpg"))
+kerasService = KerasService(
+    args.keras_host, args.keras_port, os.path.join(LOG_DIRECTORY, "screenshot.jpg"))
 
 try:
     start_time, end_time = run_benchmark()
+    steam_id = get_registry_active_user()
+    config_path = os.path.join(
+        os.environ["HOMEPATH"], "Saved Games" ,"The Last of Us Part I",
+        "users", str(steam_id), "screeninfo.cfg"
+    )
     height, width = get_resolution(config_path)
-    result = {
+    report = {
         "resolution": format_resolution(width, height),
-        "graphics_preset": "current",
         "start_time": seconds_to_milliseconds(start_time),
         "end_time": seconds_to_milliseconds(end_time)
     }
 
-    write_report_json(LOG_DIRECTORY, "report.json", result)
+    write_report_json(LOG_DIRECTORY, "report.json", report)
+#pylint: disable=broad-exception-caught
 except Exception as e:
     logging.error("Something went wrong running the benchmark!")
     logging.exception(e)
     terminate_processes(PROCESS_NAME)
-    exit(1)
+    sys.exit(1)
--- the_last_of_us_part_i/the_last_of_us_part_i_utils.py
+++ the_last_of_us_part_i/the_last_of_us_part_i_utils.py
@@ -1,16 +1,21 @@
+"""Utility functions for The Last of Us Part I test script"""
 from argparse import ArgumentParser
 import ctypes
 import re
 
 
 def get_args() -> any:
+    """Get command line arg values"""
     parser = ArgumentParser()
-    parser.add_argument("--kerasHost", dest="keras_host", help="Host for Keras OCR service", required=True)
-    parser.add_argument("--kerasPort", dest="keras_port", help="Port for Keras OCR service", required=True)
+    parser.add_argument(
+        "--kerasHost", dest="keras_host", help="Host for Keras OCR service", required=True)
+    parser.add_argument(
+        "--kerasPort", dest="keras_port", help="Port for Keras OCR service", required=True)
     return parser.parse_args()
 
 
 def get_windowed_resolution(lines: list[str]):
+    """Get windowed resolution value from local game file"""
     width_pattern = re.compile(r"WindowWidth=(\d+)")
     height_pattern = re.compile(r"WindowHeight=(\d+)")
 
@@ -31,11 +36,16 @@ def get_windowed_resolution(lines: list[str]):
 
 
 def get_borderless_resolution(lines: list[str]):
-    # Note:
-    # If resolution is set to native resolution, then the values of BorderlessWidth and BorderlessHeight saved in the config are 0, 0.
-    # Without getting into various 3rd-party gui libraries, we can grab resolution sizes from the system metrics if we get values of 0.
-    # However, this has limitations: This can only get values for the primary monitor and reportedly may not return correct values
-    # with high DPI displays.
+    """Get borderless resolution value from local game file
+
+    Note:
+    If resolution is set to native resolution, then the values of
+    BorderlessWidth and BorderlessHeight saved in the config are 0, 0.
+    Without getting into various 3rd-party gui libraries, we can grab
+    resolution sizes from the system metrics if we get values of 0.
+    However, this has limitations: This can only get values for the primary monitor
+    and reportedly may not return correct values with high DPI displays.
+    """
 
     width_pattern = re.compile(r"BorderlessWidth=(\d+)")
     height_pattern = re.compile(r"BorderlessHeight=(\d+)")
@@ -59,14 +69,14 @@ def get_borderless_resolution(lines: list[str]):
             if window_height == 0:
                 window_height = user32.GetSystemMetrics(1)
 
-    return window_height, window_width 
+    return window_height, window_width
 
 
 def get_resolution(config_path: str):
-    """
-    Gets the resolution by retrieving the active steam user id from registry to complete the path to the local config file.
-    Then parses the local config file and grabbing values depending on if the display settings were set to Windowed or
-    Borderless windowed mode.
+    """Gets the resolution by retrieving the active steam user id from
+    registry to complete the path to the local config file.
+    Then parses the local config file and grabbing values depending on
+    if the display settings were set to Windowed or Borderless windowed mode.
     """
     window_mode_pattern = re.compile(r"WindowMode=(\d)")
 
@@ -74,17 +84,17 @@ def get_resolution(config_path: str):
     window_width = 0
     window_height = 0
 
-    with open(config_path) as f:
-        window_mode_line = f.readline()
+    with open(config_path, encoding="utf-8") as file:
+        window_mode_line = file.readline()
         window_mode = window_mode_pattern.search(window_mode_line).group(1)
 
         if not window_mode:
             return window_height, window_width
-    
-        lines = f.readlines()
+
+        lines = file.readlines()
         if int(window_mode) == 1:
             window_height, window_width = get_borderless_resolution(lines)
-        else:   
+        else:
             window_height, window_width = get_windowed_resolution(lines)
-            
+
     return window_height, window_width
