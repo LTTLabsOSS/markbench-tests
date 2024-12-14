90cc75ab604028d6a083a6bca4e5b26c3a5e0796
J-Doiron <139803019+J-Doiron@users.noreply.github.com>
2024-12-13 18:31:19 -0800

    Update misc.py

--- harness_utils/misc.py
+++ harness_utils/misc.py
@@ -123,6 +123,7 @@ class LTTGamePadDS4(vg.VDS4Gamepad):
         """
 
         self.directional_pad(direction=direction)
+        self.reset()
         self.update()
 
     def dpad_press_n_times(self, direction: vg.DS4_DPAD_DIRECTIONS, n: int):
