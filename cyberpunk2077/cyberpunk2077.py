7a0bc645248b35c2f8738716934f6cfb0c663c65
J-Doiron <139803019+J-Doiron@users.noreply.github.com>
2025-02-14 16:30:56 -0800

    Update cyberpunk2077.py

--- cyberpunk2077/cyberpunk2077.py
+++ cyberpunk2077/cyberpunk2077.py
@@ -83,26 +83,29 @@ def navigate_settings() -> None:
     # now on graphics tab
     am.take_screenshot("graphics_1.png", ArtifactType.CONFIG_IMAGE, "graphics menu 1")
 
+    user.press("down")
+
+    RAST = kerasService.wait_for_word("view", interval=1, timeout=2)
+    if RAST:
+        press_n_times("up", 2, 0.2) #gets you to film grain
+
     DLSS = kerasService.wait_for_word("dlss", interval=1, timeout=2)
     if DLSS:
         result = kerasService.wait_for_word("multi", interval=1, timeout=2)
         if result:
             user.press("down")
-        press_n_times("down", 3, 0.2) #gets you to film grain usually except for combined with RT
+        press_n_times("down", 2, 0.2) #gets you to film grain usually except for combined with RT
         result = kerasService.wait_for_word("grain", interval=1, timeout=2)
         if not result:
             user.press("down")
 
     FSR = kerasService.wait_for_word("amd", interval=1, timeout=2)
     if FSR:
-        press_n_times("down", 2, 0.2) #gets you to film grain
+        user.press("down") #gets you to film grain
 
     XESS = kerasService.wait_for_word("intel", interval=1, timeout=2)
     if XESS:
-        press_n_times("down", 2, 0.2) #gets you to film grain
-
-    else:
-        user.press("down")
+        user.press("down") #gets you to film grain
 
     check_for_rt()
 
