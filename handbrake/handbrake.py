53b808fa7137401bde6c941da5af874a624ffece
Derek Hirotsu <d.hirotsu@linusmediagroup.com>
2025-07-17 16:39:06 -0700

    typo

--- handbrake/handbrake.py
+++ handbrake/handbrake.py
@@ -127,7 +127,7 @@ def main():
         if is_video_source_present() is False:
             logging.info("copying big buck bunny from network drive")
             if args.share_user is not None and args.share_name is not None:
-                connect_and_copy_video
+                connect_and_copy_video(args.share_user, args.share_pass)
             else:
                 copy_video_source()
 
