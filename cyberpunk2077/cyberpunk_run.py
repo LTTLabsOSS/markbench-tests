30090d6432cc3cf78af326964c2e0ac0dcf36d92
James <j.lin@linusmediagroup.com>
2026-01-14 17:13:03 -0800

    changes

--- cyberpunk2077/cyberpunk_run.py
+++ cyberpunk2077/cyberpunk_run.py
@@ -11,10 +11,10 @@ def run_benchmark(sc: Screenshotter, am: ArtifactManager) -> tuple[int, int]:
     if not navigate_settings(sc, am):
         return (0, 0)
 
-    if not find_word(sc, "fps", "Benchmark didn't start.", timeout = 30):
+    if not find_word(sc, "fps", "Benchmark didn't start.", timeout=30):
         return (0, 0)
 
-    test_start_time = int_time() - 5
+    test_start_time = int_time() - 4
 
     logging.info("Benchmark started. Waiting for benchmark to complete.")
 
@@ -23,11 +23,11 @@ def run_benchmark(sc: Screenshotter, am: ArtifactManager) -> tuple[int, int]:
     if not find_word(sc, "results", "Did not see results screen."):
         return (0, 0)
 
+    test_end_time = int_time()
+
     am.take_screenshot(
         "results.png", ArtifactType.RESULTS_IMAGE, "results of benchmark"
     )
-
-    test_end_time = int_time() - 2
 
     return test_start_time, test_end_time
 
@@ -37,7 +37,7 @@ def navigate_settings(sc: Screenshotter, am: ArtifactManager) -> bool:
 
     logging.info("Navigating main menu")
 
-    if not find_word(sc, "new", "Did not see main menu.", timeout = 30):
+    if not find_word(sc, "new", "Did not see main menu.", timeout=30):
         return False
 
     # navigating to settings menu
@@ -58,7 +58,9 @@ def navigate_settings(sc: Screenshotter, am: ArtifactManager) -> bool:
 
     # now on graphics tab
 
-    am.take_screenshot("2_graphics_preset.png", ArtifactType.CONFIG_IMAGE, "graphics preset")
+    am.take_screenshot(
+        "2_graphics_preset.png", ArtifactType.CONFIG_IMAGE, "graphics preset"
+    )
 
     press("down*2")
 
@@ -75,13 +77,15 @@ def navigate_settings(sc: Screenshotter, am: ArtifactManager) -> bool:
             if not find_word(sc, "crowd"):
                 # RT on, Resolution Scaling on
                 if not find_word(sc, "photo"):
-                    #DLSS
+                    # DLSS
                     press("down*3")
                 else:
                     press("down")
-                    
+
                 am.take_screenshot(
-                    "3_graphics_rt_scaling.png", ArtifactType.CONFIG_IMAGE, "graphics menu rt when scaling also enabled"
+                    "3_graphics_rt_scaling.png",
+                    ArtifactType.CONFIG_IMAGE,
+                    "graphics menu rt when scaling also enabled",
                 )
             else:
                 # RT on, Resolution Scaling off
@@ -94,21 +98,27 @@ def navigate_settings(sc: Screenshotter, am: ArtifactManager) -> bool:
     if not find_word(sc, "shadows"):
         return False
 
-    am.take_screenshot("4_graphics_basic.png", ArtifactType.CONFIG_IMAGE, "graphics basic")
+    am.take_screenshot(
+        "4_graphics_basic.png", ArtifactType.CONFIG_IMAGE, "graphics basic"
+    )
 
     press("down*10")
 
     if not find_word(sc, "dynamic"):
         return False
 
-    am.take_screenshot("5_graphics_advanced1.png", ArtifactType.CONFIG_IMAGE, "graphics advanced 1")
+    am.take_screenshot(
+        "5_graphics_advanced1.png", ArtifactType.CONFIG_IMAGE, "graphics advanced 1"
+    )
 
     press("down*6")
 
     if not find_word(sc, "level"):
         return False
 
-    am.take_screenshot("6_graphics_advanced2.png", ArtifactType.CONFIG_IMAGE, "graphics advanced 2")
+    am.take_screenshot(
+        "6_graphics_advanced2.png", ArtifactType.CONFIG_IMAGE, "graphics advanced 2"
+    )
 
     press("3")
 
