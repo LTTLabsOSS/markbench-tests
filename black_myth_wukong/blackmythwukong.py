a3d086396b081a0fe5ecebb980fbcf881478d6fc
J-Doiron <139803019+J-Doiron@users.noreply.github.com>
2026-01-16 19:22:24 -0800

    Tuning Black Myth's window

--- black_myth_wukong/blackmythwukong.py
+++ black_myth_wukong/blackmythwukong.py
@@ -160,7 +160,7 @@ def run_benchmark(keras_service):
         logging.info("Could not find current. Unable to mark start time!")
         sys.exit(1)
 
-    test_start_time = int(time.time()) - 2
+    test_start_time = int(time.time())
 
     time.sleep(142)
 
@@ -168,7 +168,7 @@ def run_benchmark(keras_service):
         logging.info("Did not find result screen. Did the benchmark run?")
         sys.exit(1)
 
-    test_end_time = int(time.time()) - 1
+    test_end_time = int(time.time()) - 4
     time.sleep(5)
     am.take_screenshot("results.png", ArtifactType.RESULTS_IMAGE, "benchmark results")
     am.copy_file(f"{CONFIG_LOCATION}\\{CONFIG_FILENAME}", ArtifactType.CONFIG_TEXT, "GameUserSettings.ini")
