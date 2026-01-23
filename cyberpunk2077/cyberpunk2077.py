c7e50410bf76d5bd376bc6c7810c73f6b4366e7e
James <j.lin@linusmediagroup.com>
2026-01-23 14:58:21 -0800

    fix cyber

--- cyberpunk2077/cyberpunk2077.py
+++ cyberpunk2077/cyberpunk2077.py
@@ -1,13 +1,14 @@
+import logging
 import sys
 from pathlib import Path
 
-from cyberpunk_run import run_benchmark
 from cyberpunk_utils import copy_no_intro_mod, start_game, write_report
 
 HARNESSES_ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(1, str(HARNESSES_ROOT))
 
-from harness_utils.artifacts import ArtifactManager
+from harness_utils.artifacts import ArtifactManager, ArtifactType
+from harness_utils.helper import find_word, int_time, press, sleep
 from harness_utils.output import setup_logging
 from harness_utils.process import terminate_processes
 
@@ -16,14 +17,140 @@ LOG_DIRECTORY = SCRIPT_DIRECTORY / "run"
 PROCESS_NAME = "cyberpunk2077.exe"
 
 
-setup_logging(LOG_DIRECTORY)
+def navigate_settings(am: ArtifactManager) -> bool:
+    """Simulate inputs to navigate the main menu"""
 
-am = ArtifactManager(LOG_DIRECTORY)
+    logging.info("Navigating main menu")
 
-copy_no_intro_mod()
-start_game()
-start_time, end_time = run_benchmark(am)
-terminate_processes(PROCESS_NAME)
-am.create_manifest()
-write_report(LOG_DIRECTORY, start_time, end_time)
-sys.exit(0)
+    if not find_word("new", "Did not see main menu.", timeout=30):
+        return False
+
+    # navigating to settings menu
+    if not find_word("continue"):
+        # an account with no save game
+        press("left,enter")
+    else:
+        press("left,down,enter")
+
+    if not find_word("volume", "Did not see settings menu"):
+        return False
+
+    # entered settings
+    press("3*3")
+
+    if not find_word("preset"):
+        return False
+
+    # now on graphics tab
+
+    am.take_screenshot("2_graphics_preset.png", ArtifactType.CONFIG_IMAGE)
+
+    press("down*2")
+
+    if not find_word("grain"):
+        if not find_word("reflections"):
+            # RT off
+            # Resolution Scaling On
+            if not find_word("field"):
+                press("down*8")
+            else:
+                press("down*7")
+        else:
+            # RT on
+            if not find_word("crowd"):
+                # RT on, Resolution Scaling on
+                if not find_word("photo"):
+                    # DLSS
+                    press("down*3")
+                else:
+                    press("down")
+
+                am.take_screenshot(
+                    "3_graphics_rt_scaling.png",
+                    ArtifactType.CONFIG_IMAGE,
+                    "graphics menu rt when scaling also enabled",
+                )
+            else:
+                # RT on, Resolution Scaling off
+                press("down*8")
+
+    else:
+        # Normal Run
+        press("down*6")
+
+    if not find_word("shadows"):
+        return False
+
+    am.take_screenshot("4_graphics_basic.png", ArtifactType.CONFIG_IMAGE)
+
+    press("down*10")
+
+    if not find_word("dynamic"):
+        return False
+
+    am.take_screenshot("5_graphics_advanced1.png", ArtifactType.CONFIG_IMAGE)
+
+    press("down*6")
+
+    if not find_word("level"):
+        return False
+
+    am.take_screenshot("6_graphics_advanced2.png", ArtifactType.CONFIG_IMAGE)
+
+    press("3")
+
+    if not find_word("resolution"):
+        return False
+
+    # now on video tab
+    am.take_screenshot("1_video.png", ArtifactType.CONFIG_IMAGE)
+
+    return True
+
+
+def run_benchmark(am: ArtifactManager) -> tuple[int, int]:
+    sleep(15)
+
+    if not navigate_settings(am):
+        logging.error("Failed to navigate settings")
+        return (0, 0)
+
+    logging.info("Starting benchmark")
+
+    press("b, enter")
+
+    if not find_word("fps", timeout=30):
+        return (0, 0)
+
+    test_start_time = int_time() - 3
+
+    logging.info("Benchmark started. Waiting for benchmark to complete.")
+
+    sleep(70)  # make into an editable const, adjust timings
+
+    if not find_word("results", "Did not see results screen."):
+        return (0, 0)
+
+    test_end_time = int_time() - 4
+
+    am.take_screenshot("results.png", ArtifactType.RESULTS_IMAGE)
+
+    return test_start_time, test_end_time
+
+
+def main():
+    setup_logging(LOG_DIRECTORY)
+
+    am = ArtifactManager(LOG_DIRECTORY)
+
+    copy_no_intro_mod()
+    start_game()
+    start_time, end_time = run_benchmark(am)
+    terminate_processes(PROCESS_NAME)
+    am.create_manifest()
+    write_report(LOG_DIRECTORY, start_time, end_time)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
--- harness_utils/ocr_service.py
+++ harness_utils/ocr_service.py
@@ -1,9 +1,10 @@
 import json
 import time
 
+import cv2
+import numpy as np
 import requests
-
-from harness_utils.screenshot import Screenshotter
+from mss.windows import MSS as mss
 
 DEFAULT_TIMEOUT = 120.0
 
@@ -13,14 +14,12 @@ class OCRService:
         self,
         ip_addr: str,
         port: int | str,
-        screenshotter: Screenshotter,
         timeout: float = DEFAULT_TIMEOUT,
     ) -> None:
         self.ip_addr = ip_addr
         self.port = str(port)
         self.url = f"http://{ip_addr}:{str(port)}/process"
         self.timeout = timeout
-        self.screenshotter = screenshotter
 
     def _query_service(self, word: str, image_bytes):
         try:
@@ -44,22 +43,28 @@ class OCRService:
     def look_for_word(
         self, word: str, attempts: int = 1, interval: float = 0.0
     ) -> bool:
-        for _ in range(attempts):
-            image_bytes = self.screenshotter.take_sc_bytes()
-            result = self._query_service(word, image_bytes)
-            if result is not None:
-                return result
-            time.sleep(interval)
+        with mss() as scr:
+            monitor_1 = scr.monitors[1]
+            for _ in range(attempts):
+                sc = np.array(scr.grab(monitor_1))
+                _, buf = cv2.imencode(".jpg", sc)
+                image_bytes = buf.tobytes()
+                result = self._query_service(word, image_bytes)
+                if result is not None:
+                    return result
+                time.sleep(interval)
         return None
 
     def wait_for_word(self, word: str, interval: float = 0.0, timeout: int = 3) -> bool:
         search_start_time = time.time()
-        while time.time() - search_start_time < timeout:
-            image_bytes = self.screenshotter.take_sc_bytes()
-            result = self._query_service(word, image_bytes)
-            if result is not None:
-                return result
-            time.sleep(interval)
+        with mss() as scr:
+            monitor_1 = scr.monitors[1]
+            while time.time() - search_start_time < timeout:
+                sc = np.array(scr.grab(monitor_1))
+                _, buf = cv2.imencode(".jpg", sc)
+                image_bytes = buf.tobytes()
+                result = self._query_service(word, image_bytes)
+                if result is not None:
+                    return result
+                time.sleep(interval)
         return None
-
-
