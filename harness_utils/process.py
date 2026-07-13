73411dfcef286d4b815b6c4dbb5531154436532b
James <j.lin@linusmediagroup.com>
2026-07-13 15:35:47 -0700

    consolidate windows and linux terminate process

--- harness_utils/process.py
+++ harness_utils/process.py
@@ -16,42 +16,22 @@ def terminate_process(process_name: str) -> None:
         return
 
     logger.debug("Terminating process: %s", process_name)
-    if is_windows():
-        for process in psutil.process_iter(["pid", "name"]):
-            process_name_current = process.info.get("name") or ""
-            if process_name.lower() in process_name_current.lower():
-                logger.debug(
-                    "Terminating process pid=%s name=%s",
-                    process.info.get("pid"),
-                    process_name_current,
-                )
-                try:
-                    process.terminate()
-                except psutil.Error as err:
-                    logger.warning("Failed to terminate process: %s", err)
-        logger.debug("Process termination complete")
-        return
+    for process in psutil.process_iter(["pid", "name", "cmdline"]):
+        command = " ".join(process.info.get("cmdline") or [])
 
-    if is_linux():
-        for process in psutil.process_iter(["pid", "name", "cmdline"]):
-            command = " ".join(process.info.get("cmdline") or [])
+        if process_name.lower() in command.lower():
+            logger.debug(
+                "Terminating process pid=%s name=%s command=%s",
+                process.info.get("pid"),
+                process.info.get("name"),
+                command,
+            )
+            try:
+                process.terminate()
+            except psutil.Error as err:
+                logger.warning("Failed to terminate process: %s", err)
 
-            if process_name.lower() in command.lower():
-                logger.debug(
-                    "Terminating process pid=%s name=%s command=%s",
-                    process.info.get("pid"),
-                    process.info.get("name"),
-                    command,
-                )
-                try:
-                    process.terminate()
-                except psutil.Error as err:
-                    logger.warning("Failed to terminate process: %s", err)
-
-        logger.debug("Process termination complete")
-        return
-
-    logger.warning("Process termination unsupported on this platform")
+    logger.debug("Process termination complete")
 
 
 def is_process_running(process_name):
