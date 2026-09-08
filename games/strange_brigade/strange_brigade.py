f015acc784081f185588fd95e18a17d36e485aeb
James <j.lin@linusmediagroup.com>
2026-09-08 14:55:46 -0700

    Update strange_brigade.py

--- games/strange_brigade/strange_brigade.py
+++ games/strange_brigade/strange_brigade.py
@@ -141,7 +141,6 @@ def run_benchmark(render_engine):
 
     capture_and_save_screenshot(ARTIFACTS_DIRECTORY / "display.png", vulkan=is_vulkan)
 
-    time.sleep(0.5)
     press("escape, down*5, enter")
 
     elapsed_setup_time = round(int(time.time()) - setup_start_time, 2)
