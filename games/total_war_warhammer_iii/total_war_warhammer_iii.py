60a9d005248b50baab9572dc574f3cb00ea1ffde
James <j.lin@linusmediagroup.com>
2026-09-10 15:52:23 -0700

    Pass launcher-skip to both Total War Windows launches

--- games/total_war_warhammer_iii/total_war_warhammer_iii.py
+++ games/total_war_warhammer_iii/total_war_warhammer_iii.py
@@ -72,7 +72,7 @@ def start_game():
     game_path = get_app_install_location(STEAM_GAME_ID)
     process_path = Path(game_path) / PROCESS_NAME
     logger.info("Starting game: %s", process_path)
-    return subprocess.Popen([process_path], cwd=game_path)
+    return subprocess.Popen([process_path, "--launcher-skip"], cwd=game_path)
 
 
 def skip_logo_screens() -> None:
