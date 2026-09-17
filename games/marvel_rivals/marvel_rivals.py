3cc1d8f51d24ef869f5fae65812f6c9e88b719fa
James <j.lin@linusmediagroup.com>
2026-09-16 17:42:24 -0700

    Update marvel_rivals.py

--- games/marvel_rivals/marvel_rivals.py
+++ games/marvel_rivals/marvel_rivals.py
@@ -106,7 +106,7 @@ def run_benchmark():
 
     capture_and_save_screenshot(ARTIFACTS_DIRECTORY / "video1.png")
     time.sleep(1)
-    scroll(-1200, 1)
+    scroll(-100, 12)
     time.sleep(0.5)
 
     result = find_word("processing", timeout=30, interval=1)
